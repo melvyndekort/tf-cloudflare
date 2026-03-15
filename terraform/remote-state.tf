d153fbc2480c22aa42fbd27724e8de8256074998
Melvyn de Kort <melvyn@mdekort.nl>
2026-03-15 11:58:40 +0100

    Update remote state bucket to mdekort-tfstate-075673041815

--- terraform/remote-state.tf
+++ terraform/remote-state.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "tf_cognito" {
   backend = "s3"
 
   config = {
-    bucket = "mdekort.tfstate"
+    bucket = "mdekort-tfstate-075673041815"
     key    = "tf-cognito.tfstate"
     region = "eu-west-1"
   }
